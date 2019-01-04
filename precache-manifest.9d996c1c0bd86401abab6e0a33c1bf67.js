0d64147a3d5b504c633b181f9a5be423e3cbc53e
Kevinn Mitchell <kevinnmitchell@Kevinns-MacBook-Pro.local>
2019-01-03 21:07:41 -0600

    Updates

---
+++ precache-manifest.9d996c1c0bd86401abab6e0a33c1bf67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ddf4bb044de5833c823c",
+    "url": "/static/js/main.ddf4bb04.chunk.js"
+  },
+  {
+    "revision": "c4c2a1785bad3432b592",
+    "url": "/static/js/1.c4c2a178.chunk.js"
+  },
+  {
+    "revision": "ddf4bb044de5833c823c",
+    "url": "/static/css/main.aff0dc94.chunk.css"
+  },
+  {
+    "revision": "c4c2a1785bad3432b592",
+    "url": "/static/css/1.8d03a239.chunk.css"
+  },
+  {
+    "revision": "82df42f29754f6082cfbbac981656265",
+    "url": "/index.html"
+  }
+];
